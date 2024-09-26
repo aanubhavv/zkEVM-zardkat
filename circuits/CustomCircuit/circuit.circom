89e5b0b1e72c074e3685298c8268bdc90d083d3e
Anubhav Garg <117589083+aanubhavv@users.noreply.github.com>
2024-09-27 01:23:54 +0530

    Add files via upload

---
+++ circuits/CustomCircuit/circuit.circom
@@ -0,0 +1,61 @@
+pragma circom 2.0.0;
+
+
+template CustomCircuit () {  
+
+   // signal inputs
+   signal input a;
+   signal input b;
+
+   // signals from gates
+   signal x;
+   signal y;
+
+   // final signal output
+   signal output q;
+
+   // component gates used to create custom circuit
+   component andGate = AND();
+   component notGate = NOT();
+   component orGate = OR();
+
+   // circuit logic
+   andGate.a <== a;
+   andGate.b <== b;
+   notGate.in <== b;
+
+   x <==andGate.out;
+   y <==notGate.out;
+
+   orGate.a <==x;
+   orGate.b <==y;
+
+   q <== orGate.out;
+   
+}
+
+template AND() {
+    signal input a;
+    signal input b;
+    signal output out;
+
+    out <== a*b;
+
+}
+
+template NOT() {
+    signal input in;
+    signal output out;
+
+    out <== 1 + in - 2*in;
+}
+
+template OR() {
+    signal input a;
+    signal input b;
+    signal output out;
+
+    out <== a + b - a*b;
+}
+
+component main = CustomCircuit();
---
+++ hardhat.config.ts
@@ -0,0 +1,48 @@
+import 'dotenv/config'
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+// https://github.com/projectsophon/hardhat-circom
+import "hardhat-circom";
+// circuits
+import circuits = require('./circuits.config.json')
+
+// set env var to the root of the project
+process.env.BASE_PATH = __dirname;
+
+// env error
+if (!process.env.PRIVATE_KEY) {
+  throw new Error("Please set your PRIVATE_KEY in a .env file");
+}
+
+// tasks
+import "./tasks/newcircuit.ts"
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.11",
+      }
+    ]
+  },
+  circom: {
+    // (optional) Base path for input files, defaults to `./circuits/`
+    inputBasePath: "./circuits",
+    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
+    ptau: "powersOfTau28_hez_final_12.ptau",
+    // (required) Each object in this array refers to a separate circuit
+    circuits: JSON.parse(JSON.stringify(circuits))
+  },
+   networks:{
+    cardona:{
+      url:process.env.POLYGON_ZKEVM_CARDONA_API_URL,
+      accounts:[process.env.PRIVATE_KEY],
+    }
+  }
+};
+
+export default config;
